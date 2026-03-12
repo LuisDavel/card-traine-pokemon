16e216dcbe76fc6c45f2b0d86bacce1b7cfbf78d
Luis Davel <42303034+LuisDavel@users.noreply.github.com>
2026-03-12 18:18:23 -0300

    ajuste

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,14 @@ import { db } from "@/lib/db";
 import * as schema from "@/lib/db/schema";
 
 export const auth = betterAuth({
+  baseURL: {
+    allowedHosts: [
+      "localhost:3000",
+      "card-traine-pokemon.vercel.app",
+      "*.vercel.app", // Aceita qualquer URL de preview da Vercel
+    ],
+    protocol: process.env.NODE_ENV === "development" ? "http" : "https",
+  },
   database: drizzleAdapter(db, {
     provider: "mysql",
     schema,
